3a0748aed64ea68d000eae274c625a4b0aeab93d
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2024-03-31 02:31:03 +0000

    16 threads at once baby!!

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage import io
+import threading
 
 def alter(arr, start, end):
     for x in arr:
@@ -11,7 +12,6 @@ def alter(arr, start, end):
         start += 1
         if (start >= end):
             return arr
-
 
 im = io.imread('IMG_1023.jpg')
 imarr=np.array(im)
@@ -24,12 +24,34 @@ whitish_pixels = np.count_nonzero((imarr[ :, :, 0] > 140) & (imarr[ :, :, 1] > 1
 print(f"{whitish_pixels} out of {size} pixels are whitish")
 print(f"{whitish_pixels / size * 100}% of the image is whitish")
 
-fraction = np.shape(im)[0] / 10
+fraction = np.shape(im)[0] / 16
 
-for i in range(9):
-    imarr = alter(imarr, i*fraction, (i+1)*fraction)
+for i in range(15):
+    print(f"Starting {i} sweep")
+    imarr = alter(imarr, i*fraction, (i+1)*fraction-1)
 
+threads = [
+    threading.Thread(target=alter, args=(imarr, 0, fraction - 1)),
+    threading.Thread(target=alter, args=(imarr, fraction, (fraction * 2) - 1)),
+    threading.Thread(target=alter, args=(imarr, (fraction * 2), (fraction * 3) - 1)),
+    threading.Thread(target=alter, args=(imarr, (fraction * 3), (fraction * 4) - 1)),
+    threading.Thread(target=alter, args=(imarr, (fraction * 4), (fraction * 5) - 1)),
+    threading.Thread(target=alter, args=(imarr, (fraction * 5), (fraction * 6) - 1)),
+    threading.Thread(target=alter, args=(imarr, (fraction * 6), (fraction * 7) - 1)),
+    threading.Thread(target=alter, args=(imarr, (fraction * 7), (fraction * 8) - 1)),
+    threading.Thread(target=alter, args=(imarr, (fraction * 8), (fraction * 9) - 1)),
+    threading.Thread(target=alter, args=(imarr, (fraction * 9), (fraction * 10) - 1)),
+    threading.Thread(target=alter, args=(imarr, (fraction * 10), (fraction * 11) - 1)),
+    threading.Thread(target=alter, args=(imarr, (fraction * 11), (fraction * 12) - 1)),
+    threading.Thread(target=alter, args=(imarr, (fraction * 12), (fraction * 13) - 1)),
+    threading.Thread(target=alter, args=(imarr, (fraction * 13), (fraction * 14) - 1)),
+    threading.Thread(target=alter, args=(imarr, (fraction * 14), (fraction * 15) - 1)),
+    threading.Thread(target=alter, args=(imarr, (fraction * 15), (fraction * 16) - 1)),
+           ]
 
+for x in threads:
+    print(f"starting thread x")
+    imarr = threads[x]
 
 
 #for x in imarr: #x index
